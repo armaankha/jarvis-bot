88c1c04c818fc28fe5c179eb2478bc93c9e06e45
armaankha <65939229+armaankha@users.noreply.github.com>
2020-05-26 11:49:17 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = ">" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
+        if(msg.startsWith('>', 0)){
+            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
+                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN)
